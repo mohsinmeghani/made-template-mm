3f2c48f80aebd48ac33393a4e058a36b7264d6a7
Mohsin <meghani.mohsin@gmail.com>
2024-06-26 11:45:27 +0200

    change test file

--- project/Tests.sh
+++ project/Tests.sh
@@ -2,16 +2,6 @@
 echo "Starting Tests"
 echo "------------------------"
 
-echo "Removing Existing Files"
-rm -f ../data/climate_change.sqlite
-
-if [ -f ../data/climate_change.sqlite ]; then
-    echo "Failed to Remove existing output files."
-    exit 1
-fi
-
-echo "Files Removed "
-
 echo "running system test."
 python ./system-test.py
 
